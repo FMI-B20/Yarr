097b2b308c93b22b249141c255ff6d8221e5ad74
cdascalu <cristiandascalu93@gmail.com>
2015-04-06 16:52:43 +0300

    added locationTypes

--- frontend/js/controllers/settings.js
+++ frontend/js/controllers/settings.js
@@ -1,3 +1,26 @@
-yarr.controller('SettingsController', ['$scope', 'Cuisines', function($scope, Cuisines) {
+yarr.controller('SettingsController', ['$scope', 'Cuisines', 'LocationTypes', function($scope, Cuisines, LocationTypes) {
+  
   $scope.cuisines = Cuisines.query();
+  $scope.locationTypes = LocationTypes.query();
+
+  $scope.save = function() {
+  	
+  	//selected cuisines
+  	var cuisinesArr = [];
+  	$.each($scope.cuisines, function(index, cuisine) {
+  		
+  		if(cuisine.selected && cuisine.selected == true)
+  			cuisinesArr.push(cuisine.id);
+  	});
+
+  	//selected locationTypes
+  	var locationTypesArr = [];
+  	$.each($scope.locationTypesArr, function(index, locationType) {
+
+  		if(locationType.selected && locationType.selected == true)
+  			locationTypesArr.push(locationType.id);
+
+  	});
+
+  };
 }]);
