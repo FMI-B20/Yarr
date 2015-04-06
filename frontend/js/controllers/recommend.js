6b6ba090dd59d1a4e93b892d7e41c8b7bf2e9aed
cdascalu <cristiandascalu93@gmail.com>
2015-04-06 19:43:23 +0300

    new way to send infromations from settings

---
+++ frontend/js/controllers/recommend.js
@@ -0,0 +1,9 @@
+yarr.controller('RecommendController', ['$scope', '$stateParams', 
+    function($scope, $stateParams) {
+  
+  var cuisinesArr = $stateParams.cuisines.split(',');
+  var locationTypesArr = $stateParams.locationTypes.split(',');
+
+  
+
+}]);
