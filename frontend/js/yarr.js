80390759e73b2cd70d8a745d434ed8511cc06985
Gabi Purcaru <gabi@purcaru.com>
2015-04-07 21:22:00 +0300

    replaced checbox list with ui-select multiple

--- frontend/js/yarr.js
+++ frontend/js/yarr.js
@@ -1,4 +1,6 @@
-var yarr = angular.module('yarr', ['ui.router', 'ui.bootstrap', 'ngResource', 'djangoRESTResources', 'ngMap']);
+var yarr = angular.module('yarr', [
+  'ui.router', 'ui.bootstrap', 'ngResource', 'djangoRESTResources', 'ngMap', 'ui.select'
+]);
 yarr.config(['$resourceProvider', function($resourceProvider) {
   // Don't strip trailing slashes from calculated URLs
   $resourceProvider.defaults.stripTrailingSlashes = false;
