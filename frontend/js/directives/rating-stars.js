54c7f46b9ce6bb1bc772bf4862babb3fd47d0a53
Botezatu Mihai Catalin <botezatu.mihaicatalin@gmail.com>
2015-05-25 18:15:18 +0300

    Separated attributes in rating-stars directive

--- frontend/js/directives/rating-stars.js
+++ frontend/js/directives/rating-stars.js
@@ -3,7 +3,23 @@ yarr.directive('ratingStarsOptions', function() {
         restrict: 'A',
         link: function(scope, element, attrs) {
             $(element).rating('create', scope.$eval(attrs.ratingStarsOptions));
+        }
+    };
+});
+
+yarr.directive('ratingStarsValue', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
             $(element).rating('update', attrs.ratingStarsValue);
+        }
+    };
+});
+
+yarr.directive('ratingStarsOnRatingChange', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
             $(element).on('rating.change', scope.$eval(attrs.ratingStarsOnRatingChange));
         }
     };
