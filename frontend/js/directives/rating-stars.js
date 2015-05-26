a953010e2a035c1c2285f63c4a1d9322cc0507a6
Botezatu Mihai Catalin <botezatu.mihaicatalin@gmail.com>
2015-05-26 23:38:00 +0300

    Added show caption attribute at ratng-stars

--- frontend/js/directives/rating-stars.js
+++ frontend/js/directives/rating-stars.js
@@ -8,7 +8,7 @@ yarr.directive('ratingstars', function($parse){
 
         	// linking function here.
 	      	return function(scope,elem,attrs) {
-	        	angular.element(elem).rating('create', {disabled: attrs.inputDisabled == "true", showClear : attrs.showClear == "true", size : attrs.size, step : attrs.step, min : attrs.min, max : attrs.max});
+	        	angular.element(elem).rating('create', {disabled: attrs.inputDisabled == "true", showClear : attrs.showClear == "true", showCaption : attrs.showCaption == "true", size : attrs.size, step : attrs.step, min : attrs.min, max : attrs.max});
             	scope.$watch(modelAccessor, function (value) {
                		angular.element(elem).rating('update', value);
             	});
